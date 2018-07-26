442714c30441f452039753b3f204d93d80ebe3b4
raj-wk <raj.muthukkannan@wolterskluwer.com>
2018-07-26 16:36:02 -0400

    try _count

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -18,7 +18,8 @@
                         $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
                               'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
                               'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
-                      }
+                      },
+                    _count:"1000"
                     }
                   });
 
